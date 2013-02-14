b878b5936c7f69b44622195784e2c0133e911475
chandrakala.Gattu <cgatu@raygroupintl.com>
2013-02-14 14:51:28 -0500

    Updated code

--- main/src/gov/va/bbmi/automatedtesting/suites/example/Directemail.java
+++ main/src/gov/va/bbmi/automatedtesting/suites/example/Directemail.java
@@ -39,48 +39,40 @@ public class Directemail extends AutomatedTestingSuite {
 		driver.manage().window().maximize();
 		logger.info("Directemail::SubmitButton was clicked");
 		r.delay(5000);
+		//Click on subject
 		s.wait("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/ZlarllnlnnvF.png");
 		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/ZlarllnlnnvF.png"); //TODO: will need to refer to this on the class path and not the actual path
 		logger.info("::Sikuli Click on Subject RadiologyReport::");
 		r.delay(5000);
-		/* for (int second = 0;; second++) {
-		    	//if (second >= 60) fail("timeout");
-		    	try { if (isElementPresent(By.cssSelector("a.rowLink > span"))) break; } catch (Exception e) {System.out.println("in first exception block"); e.printStackTrace(); }
-		    	//Thread.sleep(1000);
-		    }
-
-		    for (int second = 0;; second++) {
-		    	//if (second >= 60) fail("timeout");
-		    	try { if ("Download".equals(driver.findElement(By.cssSelector("a.rowLink > span")).getText())) break; } catch (Exception e) {System.out.println("in second exception block"); e.printStackTrace();}
-		    	//Thread.sleep(1000);
-		    }*/
-
-		  // try{ driver.findElement(By.cssSelector("a.rowLink > span")).click();}catch(Exception e){System.out.println("In third exception block");e.printStackTrace();}
-		//r.delay(20000);
 		
-		//String content	=	driver.findElement(By.id("messageBody")).getText();
-		//System.out.println("content is ---"+ content);
-		//System.out.println("pagesource is --->"+driver.getPageSource());
-		//Region region 	=	s.find("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/hccp15814721.png");
-		//System.out.println("Text is -->"+region.text());
 		
 		//open pdf in email
-		//logger.info(driver.switchTo().)
+		driver.switchTo().frame(1);
+		driver.findElement(By.cssSelector("a.rowLink > span")).click();
 		
-		logger.info(driver.getWindowHandles().toString());
-		Match content = s.wait("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/hccp15814721.png");
-		String contentstring	= content.text();
-		logger.info(contentstring);
+		//Ok button for downlaod
+		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/OK.png"); //TODO: will need to refer to this on the class path and not the actual path
+		logger.info("::Sikuli Click on OK Image::");
+		r.delay(10000);
+		
+		//close pdf
+		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/E.png");
+	   	logger.info("::Sikuli Click on CLOSE Image::");
+	   	r.delay(5000);
+		
+		
+		//Clickon download link on email
+		s.wait("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/hccp15814721.png");
 		r.delay(5000);
 		//s.hover("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/hccp15814721.png"); //TODO: will need to refer to this on the class path and not the actual path
 		//r.delay(5000);
    		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/hccp15814721.png"); //TODO: will need to refer to this on the class path and not the actual path
    		r.delay(2000);
 		logger.info("::Sikuli Click on Download link::");
+		
+		// Click on Download File zip
 		System.out.println("Current url is "+driver.getCurrentUrl());
-		// driver.findElement(By.cssSelector("a.rowLink > span")).click();
-		r.delay(5000);
-		logger.info("::Sikuli waiting to  load Download Button ::");
+		// driver.findElement(By.cssSelector("a.rowLink > span")).click();		
 		String mainTab = driver.getWindowHandles().toArray()[0].toString();
 		String otherTab = driver.getWindowHandles().toArray()[1].toString();
 		logger.info(driver.getWindowHandles().toString());
@@ -88,32 +80,23 @@ public class Directemail extends AutomatedTestingSuite {
 		logger.info("Current window handle: " + driver.getWindowHandle().toString());
 		//driver.switchTo().window(arg0);
 		logger.info(driver.getTitle());
-		 driver.findElement(By.xpath("//a[@class='regular']")).click();//By.linkText("Download File")).click();
-//		s.wait("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/DmDownload.png");
-//		driver.findElement(By.("Download")).click();
-//		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/DmDownload.png"); //TODO: will need to refer to this on the class path and not the actual path
-		
+		driver.findElement(By.xpath("//a[@class='regular']")).click();
+		logger.info("::Cick on Download File zip ::"); //By.linkText("Download File")).click();
 		r.delay(10000);
-		//Ok button for downlaod
+
+		//Firefox Ok button for downlaod
 		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/OK.png"); //TODO: will need to refer to this on the class path and not the actual path
 		logger.info("::Sikuli Click on OK Image::");
 		r.delay(10000);
-		/*//Close button for download
 		
-		s.wait("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/E.png");
-		logger.info(":; Sikuli  Wait for Close image loaded::");
-		r.delay(10000);
-		
-		s.click("C:/BBMI-Automation-testing-framework-master/BBMI-Automation-testing-framework-master/main/src/Img1/E.png");
-		logger.info("::Sikuli Click on CLOSE Image::");
-		r.delay(10000);*/
-		
+		//Sikuli Click on mhv prototype folder
 		s.wait("C:/Direct mail sikuli images.sikuli/IHEES2FXEEEE.png");
 		logger.info(":; Sikuli  Wait for mhv prototype folder loaded::");
 		r.delay(5000);
 		s.doubleClick("C:/Direct mail sikuli images.sikuli/IHEES2FXEEEE.png");
 		logger.info("::Sikuli Click on mhv prototype folder Image::");
 		r.delay(5000);
+		
 		
 		s.wait("C:/Direct mail sikuli images.sikuli/mhv_protctyp.png");
 		logger.info("::Sikuli wait for mhv prototype notepad Image::");
@@ -164,16 +147,17 @@ public class Directemail extends AutomatedTestingSuite {
 		logger.info("::Sikuli Click on zip CLOSE Image::");
 		r.delay(5000);
 		
-		// switch to main tab
+		// switch to main tab and logout		
 		driver.switchTo().window(mainTab);
+		logger.info("::switch to main tab::");
+		driver.switchTo().frame(1);
+		logger.info("::switch to email message frame::");
+		r.delay(3000);
+		driver.findElement(By.xpath("//a[@class='signoutButton']")).click();
+		logger.info("::Click on signout button::");
+		r.delay(5000);
 		
-	//	driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
-		// Warning: verifyTextPresent may require manual changes
-		/*try {
-			assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Error\r\nThere was an error retrieving your download\r\n\r\nThere was an error retrieving your file for download\\.[\\s\\S]*$"));
-		} catch (Error e) {
-			verificationErrors.append(e.toString());
-		}*/
+	
 	}
 	
 }
